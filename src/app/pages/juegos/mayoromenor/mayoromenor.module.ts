1b7367e2536d6706b32bec1dc9284552779f12de
LeoLedesma <leofledesma@gmail.com>
2023-10-05 18:48:53 -0300

    mayor menor styles

--- src/app/pages/juegos/mayoromenor/mayoromenor.module.ts
+++ src/app/pages/juegos/mayoromenor/mayoromenor.module.ts
@@ -3,18 +3,25 @@ import { CommonModule } from '@angular/common';
 
 
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 import { MayoromenorRoutingModule } from './mayoromenor-routing.module';
 import { MayoromenorComponent } from './mayoromenor.component';
 
 
 @NgModule({
-  declarations: [    
+  declarations: [
     MayoromenorComponent
   ],
   imports: [
     CommonModule,
     MatCardModule,
-    MayoromenorRoutingModule
+    MayoromenorRoutingModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+
   ]
 })
 export class MayoromenorModule { }
